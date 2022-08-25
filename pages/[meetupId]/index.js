58d57d19a40f89aeae4dfc8ff542143af7a5b12a
Dao Van Dat <daovandat2k15@gmail.com>
2022-08-25 19:54:19 +0700

    339. Working with Params for SSG Data Fetching

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -12,4 +12,25 @@ function index(props) {
   );
 }
 
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  const meetupId = context.prams.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+        id: meetupId,
+        title: "A first Meetup",
+        address: "Some street 5, Some City",
+        description: "The meetup description",
+      },
+    },
+  };
+}
+
 export default index;
