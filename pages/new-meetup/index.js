8fc5325006aa01a7ecd55ede194cf8152ce95fb9
Dao Van Dat <daovandat2k15@gmail.com>
2022-08-26 09:03:33 +0700

    343. Sending Http Requests To Our API Routes

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,11 +1,25 @@
 import React from "react";
 import NewMeetupForm from "./../../components/meetups/NewMeetupForm";
+import { useRouter } from "next/router";
 
 index.propTypes = {};
 
 function index(props) {
-  const addNewMeetUpHandler = (enteredMeetupData) => {
-    console.log(enteredMeetupData);
+  const router = useRouter();
+  const addNewMeetUpHandler = async (enteredMeetupData) => {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    router.push("/"); 
   };
 
   return <NewMeetupForm onAddMeetup={addNewMeetUpHandler}></NewMeetupForm>;
